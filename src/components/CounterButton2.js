d0643fd89d4007bc1d33d04fb4d298f1ee974cfd
martin <martin@MacBook-Pro.local>
2020-07-07 14:51:29 +0300

    prittier

--- src/components/CounterButton2.js
+++ src/components/CounterButton2.js
@@ -4,28 +4,20 @@ class CounterButton extends Component {
     constructor() {
         super();
         
-
-
         this.state = {
-
             count: 0
-
         };
     }
     
     shouldComponentUpdate(nextProps, nextState) {
-        if(this.state.count !== 
-            nextState.count) {
-            
+        if(this.state.count !== nextState.count) {
             return true;
         }
-
         return false;
     }
     
     updateCount = () => {
-        this.setState(state => 
-            { 
+        this.setState(state => { 
             return {count: state.count + 1 };
         }); 
     }
